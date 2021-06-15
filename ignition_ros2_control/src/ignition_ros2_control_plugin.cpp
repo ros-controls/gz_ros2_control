c917a8c42c7cdb3eb83a4d1b8c9f73f1f9fab77c
ahcorde <ahcorde@gmail.com>
2021-06-15 17:03:36 +0200

    Forward sdf ros remappings to loaded controllers
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- ignition_ros2_control/src/ignition_ros2_control_plugin.cpp
+++ ignition_ros2_control/src/ignition_ros2_control_plugin.cpp
@@ -239,6 +239,36 @@ void IgnitionROS2ControlPlugin::Configure(
   }
 
   std::vector<std::string> arguments = {"--ros-args", "--params-file", paramFileName};
+  auto sdfPtr = const_cast<sdf::Element *>(_sdf.get());
+
+  if (sdfPtr->HasElement("ros")) {
+     sdf::ElementPtr sdfRos = sdfPtr->GetElement("ros");
+
+     // Set namespace if tag is present
+     if (sdfRos->HasElement("namespace")) {
+       std::string ns = sdfRos->GetElement("namespace")->Get<std::string>();
+       // prevent exception: namespace must be absolute, it must lead with a '/'
+       if (ns.empty() || ns[0] != '/') {
+         ns = '/' + ns;
+       }
+       std::string ns_arg = std::string("__ns:=") + ns;
+       arguments.push_back(RCL_REMAP_FLAG);
+       arguments.push_back(ns_arg);
+     }
+
+     // Get list of remapping rules from SDF
+     if (sdfRos->HasElement("remapping")) {
+       sdf::ElementPtr argument_sdf = sdfRos->GetElement("remapping");
+
+       arguments.push_back(RCL_ROS_ARGS_FLAG);
+       while (argument_sdf) {
+         std::string argument = argument_sdf->Get<std::string>();
+         arguments.push_back(RCL_REMAP_FLAG);
+         arguments.push_back(argument);
+         argument_sdf = argument_sdf->GetNextElement("remapping");
+       }
+     }
+   }
 
   std::vector<const char *> argv;
   for (const auto & arg : arguments) {
