d3116add6abf794ebcd625aa4588ed3f83a18e40
Denis Štogl <denis@stogl.de>
2022-11-25 08:51:41 +0100

    Force setting use_sim_time parameter when using plugin.

--- ign_ros2_control/src/ign_ros2_control_plugin.cpp
+++ ign_ros2_control/src/ign_ros2_control_plugin.cpp
@@ -428,6 +428,9 @@ void IgnitionROS2ControlPlugin::Configure(
     std::chrono::duration_cast<std::chrono::nanoseconds>(
       std::chrono::duration<double>(1.0 / static_cast<double>(this->dataPtr->update_rate))));
 
+  // Force setting of use_sime_time parameter
+  this->dataPtr->controller_manager_->set_parameter(rclcpp::Parameter("use_sim_time", rclcpp::ParameterValue(true)));
+
   this->dataPtr->entity_ = _entity;
 }
 
