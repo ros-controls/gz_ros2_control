4e648bd092a59e2a09aed24da397e3cc7ad592a1
Jakub Delicat <109142865+delihus@users.noreply.github.com>
2023-12-27 11:51:35 +0100

    Update diff_drive_example.launch.py (#207)
    (cherry picked from commit e20382adc627e609e277c45e74b21f603e629675)

--- gz_ros2_control_demos/launch/diff_drive_example.launch.py
+++ gz_ros2_control_demos/launch/diff_drive_example.launch.py
@@ -67,7 +67,7 @@ def generate_launch_description():
         output='screen'
     )
 
-    load_joint_trajectory_controller = ExecuteProcess(
+    load_diff_drive_controller = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
              'diff_drive_base_controller'],
         output='screen'
@@ -98,7 +98,7 @@ def generate_launch_description():
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=load_joint_state_controller,
-                on_exit=[load_joint_trajectory_controller],
+                on_exit=[load_diff_drive_controller],
             )
         ),
         node_robot_state_publisher,
