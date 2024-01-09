ef7deefa7aa6e54a24dd99eaa88da2b6f89dd885
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-01-09 10:28:38 +0100

    Use parameters with ros_gz_sim::Create (#211)
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    Co-authored-by: Christoph Fröhlich <christophfroehlich@users.noreply.github.com>

--- gz_ros2_control_demos/launch/gripper_mimic_joint_example.launch.py
+++ gz_ros2_control_demos/launch/gripper_mimic_joint_example.launch.py
@@ -58,9 +58,9 @@ def generate_launch_description():
         package='ros_gz_sim',
         executable='create',
         output='screen',
-        arguments=['-string', doc.toxml(),
-                   '-name', 'gripper',
-                   '-allow_renaming', 'true'],
+        parameters=[{'string': doc.toxml(),
+                     'name': 'gripper',
+                     'allow_renaming': True}],
     )
 
     load_joint_state_broadcaster = ExecuteProcess(
