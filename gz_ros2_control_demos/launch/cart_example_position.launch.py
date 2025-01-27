6cd21fae2bf88163219b704922b27c806cc78b17
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2025-01-27 09:26:07 +0100

    Use files from demos for testing (#485)

--- gz_ros2_control_demos/launch/cart_example_position.launch.py
+++ gz_ros2_control_demos/launch/cart_example_position.launch.py
@@ -26,6 +26,7 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     # Launch Arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    gz_args = LaunchConfiguration('gz_args', default='')
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -93,7 +94,7 @@ def generate_launch_description():
                 [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
                                        'launch',
                                        'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]),
+            launch_arguments=[('gz_args', [gz_args, ' -r -v 1 empty.sdf'])]),
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=gz_spawn_entity,
