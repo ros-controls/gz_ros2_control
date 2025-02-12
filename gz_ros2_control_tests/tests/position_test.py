656e6f33d768f838bb00ea92efa78d44fb9c7c90
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2025-02-12 09:53:56 +0100

    Change the order of tests to fix timing issues (#498)
    (cherry picked from commit 60c130a5870e37d2ff82ce57363069cf9931a2b6)

--- gz_ros2_control_tests/tests/position_test.py
+++ gz_ros2_control_tests/tests/position_test.py
@@ -85,12 +85,6 @@ class TestFixture(unittest.TestCase):
         with WaitForTopics(topic_list, timeout=10.0):
             print('/clock is receiving messages!')
 
-    def test_controller_running(self, proc_output):
-
-        cnames = ['joint_trajectory_controller', 'joint_state_broadcaster']
-
-        check_controllers_running(self.node, cnames)
-
     def test_check_if_msgs_published(self):
         check_if_js_published(
             '/joint_states',
@@ -100,6 +94,10 @@ class TestFixture(unittest.TestCase):
         )
 
     def test_arm(self, launch_service, proc_info, proc_output):
+
+        # Check if the controllers are running
+        cnames = ['joint_trajectory_controller', 'joint_state_broadcaster']
+        check_controllers_running(self.node, cnames)
 
         proc_action = Node(
             package='gz_ros2_control_demos',
