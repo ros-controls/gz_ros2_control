c05345b31c73c91fe67b0938dba8d837e8ce707e
Aarav Gupta <amronos275@gmail.com>
2025-03-10 08:49:16 +0530

    Cleanup code and use composable nodes for Gazebo - diff_drive_controller

--- gz_ros2_control_demos/launch/diff_drive_example_namespaced.launch.py
+++ gz_ros2_control_demos/launch/diff_drive_example_namespaced.launch.py
@@ -12,50 +12,77 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from ros_gz_bridge.actions import RosGzBridge
+from ros_gz_sim.actions import GzServer
+import xacro
 
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('gz_ros2_control_demos')
+
     # Launch Arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
-            PathJoinSubstitution(
-                [FindPackageShare('gz_ros2_control_demos'),
-                 'urdf', 'test_diff_drive.xacro.urdf']
+    def robot_state_publisher(context):
+        description_format = LaunchConfiguration('description_format').perform(context)
+        # Get URDF or SDF via xacro
+        xacro_processed = xacro.process(
+            os.path.join(
+                pkg_share,
+                description_format,
+                f'test_diff_drive.xacro.{description_format}'
             ),
-            ' ',
-            'namespace:=r1',
-        ]
-    )
-    robot_description = {'robot_description': robot_description_content}
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare('gz_ros2_control_demos'),
-            'config',
-            'diff_drive_controller.yaml',
-        ]
+            mappings={"namespace": "r1"}
+        )
+        node_robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace='r1',
+            output='screen',
+            parameters=[{'robot_description': xacro_processed}]
+        )
+        return [node_robot_state_publisher]
+
+    robot_controllers = os.path.join(
+        pkg_share,
+        'config',
+        'diff_drive_controller.yaml'
     )
 
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        namespace='r1',
-        output='screen',
-        parameters=[robot_description]
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '-c', '/r1/controller_manager'
+        ],
     )
+    diff_drive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'diff_drive_controller',
+            '--param-file',
+            robot_controllers,
+            '-c', '/r1/controller_manager'
+        ],
+    )
+
+    # Launch just the Gazebo server as a composable node.
+    gz_server = GzServer(
+        world_sdf_file='empty.sdf',
+        container_name='ros_gz_container',
+        create_own_container='True',
+        use_composition='True',
+    )
+
+    gz_gui = ExecuteProcess(cmd=['gz', 'sim', '-g'], output='screen')
 
     gz_spawn_entity = Node(
         package='ros_gz_sim',
@@ -66,59 +93,32 @@ def generate_launch_description():
                    'diff_drive', '-allow_renaming', 'true'],
     )
 
-    joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            'joint_state_broadcaster',
-            '-c', '/r1/controller_manager'
-            ],
-    )
-    diff_drive_base_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            'diff_drive_base_controller',
-            '--param-file',
-            robot_controllers,
-            '-c', '/r1/controller_manager'
-            ],
+    # Setup ros_gz_bridge to bridge topics between ROS and Gazebo.
+    # It is launched as a composable node in the container created by the Gazebo server.
+    ros_gz_bridge = RosGzBridge(
+        bridge_name='ros_gz_bridge',
+        config_file=os.path.join(pkg_share, 'config', 'ros_gz_bridge_config.yaml'),
+        container_name='ros_gz_container',
+        create_own_container='False',
+        use_composition='True',
     )
 
-    # Bridge
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-        output='screen'
-    )
-
-    return LaunchDescription([
-        # Launch gazebo environment
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-                                       'launch',
-                                       'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [' -r -v 1 empty.sdf'])]),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[joint_state_broadcaster_spawner],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[diff_drive_base_controller_spawner],
-            )
-        ),
-        bridge,
-        node_robot_state_publisher,
+    ld = LaunchDescription([
+        gz_server,
+        gz_gui,
         gz_spawn_entity,
+        ros_gz_bridge,
+        joint_state_broadcaster_spawner,
+        diff_drive_controller_spawner,
         # Launch Arguments
         DeclareLaunchArgument(
             'use_sim_time',
             default_value=use_sim_time,
             description='If true, use simulated clock'),
+        DeclareLaunchArgument(
+            'description_format',
+            default_value='sdf',
+            description='Robot description format to use, urdf or sdf'),
     ])
+    ld.add_action(OpaqueFunction(function=robot_state_publisher))
+    return ld
