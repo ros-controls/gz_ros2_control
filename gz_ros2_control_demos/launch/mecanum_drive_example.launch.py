3f146cba02e4a7e3d0b60efdee5ed4b13d453fb1
louis <lgparizeau@gmail.com>
2025-04-04 05:50:18 -0400

    changed controller-ros-args arguments to use append method

--- gz_ros2_control_demos/launch/mecanum_drive_example.launch.py
+++ gz_ros2_control_demos/launch/mecanum_drive_example.launch.py
@@ -76,6 +76,7 @@ def generate_launch_description():
             robot_controllers,
             '--controller-ros-args',
             '-r /mecanum_drive_controller/tf_odometry:=/tf',
+            '--controller-ros-args',
             '-r /mecanum_drive_controller/reference:=/cmd_vel',
         ],
     )
