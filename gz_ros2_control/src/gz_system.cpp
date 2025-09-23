d3bb9df693eb87f92d777bb8f060827109ed63ed
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-09-23 12:28:09 +0200

    Suppress warning (#679)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    (cherry picked from commit 64692dbec3fa9d4f0741f0eaadf2bc8628cb9774)
    
    # Conflicts:
    #	gz_ros2_control/src/gz_system.cpp

--- gz_ros2_control/src/gz_system.cpp
+++ gz_ros2_control/src/gz_system.cpp
@@ -578,6 +578,7 @@ void GazeboSimSystem::registerSensors(
 CallbackReturn
 GazeboSimSystem::on_init(const hardware_interface::HardwareInfo & info)
 {
+<<<<<<< HEAD
   if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
     return CallbackReturn::ERROR;
   }
@@ -597,6 +598,27 @@ GazeboSimSystem::on_init(const hardware_interface::HardwareInfo & info)
       "</gazebo>"
     );
   }
+=======
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+  if (hardware_interface::SystemInterface::on_init(info) !=
+    CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+  return CallbackReturn::SUCCESS;
+#ifdef _MSC_VER
+#pragma warning(pop)
+#else
+#pragma GCC diagnostic pop
+#endif
+}
+>>>>>>> 64692db (Suppress warning (#679))
 
   return CallbackReturn::SUCCESS;
 }
