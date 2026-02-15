c33263b6edd698b551ec57697a72d7ce8cdb6652
José Luis Pérez Martín <vanjoseluis@gmail.com>
2026-02-15 20:23:06 +0100

    Update gz_ros2_control_demos/launch/pendulum_example_effort.launch.py
    Co-authored-by: Christoph Fröhlich <christophfroehlich@users.noreply.github.com>

--- gz_ros2_control_demos/launch/pendulum_example_effort.launch.py
+++ gz_ros2_control_demos/launch/pendulum_example_effort.launch.py
@@ -92,7 +92,7 @@ def generate_launch_description():
                 [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
                                        'launch',
                                        'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [gz_args, ' --headless-rendering -s -r -v 1 empty.sdf'])]),
+            launch_arguments=[('gz_args', [gz_args, ' -r -v 1 empty.sdf'])]),
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=gz_spawn_entity,
