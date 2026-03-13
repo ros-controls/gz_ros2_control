e40a922854e8f17199a9472e644e53f0399cd090
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-03-13 14:13:17 +0100

    Precompute interface names
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- gz_ros2_control/src/gz_ros2_control_plugin.cpp
+++ gz_ros2_control/src/gz_ros2_control_plugin.cpp
@@ -211,8 +211,8 @@ GazeboSimROS2ControlPluginPrivate::GetEnabledJoints(
       case sdf::JointType::FIXED:
         {
           RCLCPP_INFO(
-            node_->get_logger(), "[gz_ros2_control] Fixed joint ['%s'] (Entity='%s') is skipped.",
-            jointName.c_str(), std::to_string(jointEntity).c_str()
+            node_->get_logger(), "[gz_ros2_control] Fixed joint ['%s'] (Entity='%lu') is skipped.",
+            jointName.c_str(), static_cast<unsigned long>(jointEntity)
           );
           continue;
         }
@@ -223,9 +223,9 @@ GazeboSimROS2ControlPluginPrivate::GetEnabledJoints(
         {
           RCLCPP_WARN(
             node_->get_logger(),
-            "[gz_ros2_control] Joint ['%s'] (Entity='%s') is of unsupported type."
+            "[gz_ros2_control] Joint ['%s'] (Entity='%lu') is of unsupported type."
             " Only joints with a single axis are supported.",
-            jointName.c_str(), std::to_string(jointEntity).c_str()
+            jointName.c_str(), static_cast<unsigned long>(jointEntity)
           );
           continue;
         }
@@ -464,7 +464,7 @@ void GazeboSimROS2ControlPlugin::Configure(
     RCLCPP_WARN(
       this->dataPtr->node_->get_logger(),
       "Waiting RM to load and initialize hardware...");
-    std::this_thread::sleep_for(std::chrono::microseconds(2000000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
   }
 
   this->dataPtr->entity_ = _entity;
